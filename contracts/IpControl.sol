97c2cd0f496de466289f84115c0fb1e71b16d88f
fanhua <3184107533@qq.com>
2019-04-20 13:55:31 +0800

    t

--- contracts/IpControl.sol
+++ contracts/IpControl.sol
@@ -7,6 +7,7 @@ contract IpControl{
         uint256 Number;  //设备唯一标识
         string Ip;  //ip位置
         address User;  //用户账户
+        string Area;  //地区
     }
     address public owner = msg.sender;
     uint16 private length = 100;
@@ -27,28 +28,31 @@ contract IpControl{
         owner = _owner;
     }
 
-    function set_ip(uint256 number, string memory ip, address user) public{
+    function set_ip(uint256 number, string memory ip, address user, string memory area) external{
         IpUser memory _ipUser = IpUser({
             Number: number,
             Ip: ip,
-            User: user
+            User: user,
+            Area: area
         });
         ip_save.push(_ipUser);
         if(ip_save.length % length == 0) emit IpIfOk(length);
     }
 
-    function getIp() public view onlyOwner returns(uint256[] memory, string[] memory, address[] memory){
+    function getIp() external view onlyOwner returns(uint256[] memory, string[] memory, address[] memory, string[]){
         uint256[] memory _number = new uint256[](length);
         string[] memory _ip = new string[](length);
         address[] memory _user = new address[](length);
+        string[] memory _area = new string[](length);
         uint256[] memory i = new uint256[](2);
         i[1] = start + length;
         for(i[0] = start; i[0] < i[1]; i[0]++){
             _number[i[0] - start] = ip_save[i[0]].Number;
             _ip[i[0] - start] = ip_save[i[0]].Ip;
             _user[i[0] - start] = ip_save[i[0]].User;
+            _area[i[0] - start] = ip_save[i[0]].Area;
         }
-        return (_number, _ip, _user);
+        return (_number, _ip, _user, _area);
     }
 
     function set_start() public onlyOwner {
