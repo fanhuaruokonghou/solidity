d17657b8ce3e37b296ca9566f813569ac6dbf3c2
fanhua <3184107533@qq.com>
2019-04-29 20:00:56 +0800

    last

--- contracts/TxControl.sol
+++ contracts/TxControl.sol
@@ -5,14 +5,24 @@ import './DataControl.sol';
 import './IpControl.sol';
 
 contract TxControl is transaction{
+    // struct txSave{  //非定制数据数据交易
+    //     address buyer;  //买家
+    //     address seller;  //卖家
+    //     uint256[] fileNumberList;  //文件序号列表
+    //     string publicKeyCheck;  //公钥及校验
+    //     uint8 txType;  //交易类型 1表示拥有权 2表示使用权 
+    //     bool txStatus;  //交易状态  
+    //     uint256 value;  //交易金额
+    //     uint8 buyerGrade;  //买家等级
+    // } 
+
     struct txSave{  //非定制数据数据交易
         address buyer;  //买家
         address seller;  //卖家
-        uint256[] fileNumberList;  //文件序号列表
-        string publicKeyCheck;  //公钥及校验
         uint8 txType;  //交易类型 1表示拥有权 2表示使用权 
         bool txStatus;  //交易状态  
         uint256 value;  //交易金额
+        uint8 buyerGrade;  //买家等级
     } 
 
     struct txSaveRealTime{  //定制数据数据交易
@@ -22,6 +32,9 @@ contract TxControl is transaction{
         bool txStatus;  //交易状态  
         uint256 value;  //交易金额
         uint32 accountsNumber;  //设置可接受的节点数量
+        uint8 buyerGrade;  //买家等级
+        uint64 duration;  //时长
+        uint256 buyerId;
     }
 
     mapping(address => address[]) public collectionAddress;  //收款地址列表
@@ -30,7 +43,7 @@ contract TxControl is transaction{
     txSaveRealTime[] txRealTimeList;  //定制数据交易列表
     // uint256 txPending = 0;  //交易
     event addTx(address seller, uint256[] fileNUmberList, string publicKeyCheck);  //添加非定制数据交易时触发
-    event addRealTimeTx(string publicKeyCheck, string ipOrEigenvalues);
+    event addRealTimeTx(uint256 buyerId, string publicKeyCheck, string ipOrEigenvalues, uint64 duration);
     event confirmTx(uint256 i);  //确认非定制数据交易
     event confirmRealTimeTx(uint256 i);  //确认定制数据交易
 
@@ -57,64 +70,8 @@ contract TxControl is transaction{
         require(_balances[buyer] > value * accountsNumber);
         _;
     }
-    //添加非定制数据交易
-    function buyData(address _seller, uint256[] memory _fileNumberList, string memory _publicKeyCheck, uint8 _txType, uint256 _value) public checkBalanceOfBuyData(msg.sender, _value) returns(uint256){
-         txSave memory txData = txSave({
-            buyer: msg.sender,
-            seller: _seller,
-            fileNumberList: _fileNumberList,
-            publicKeyCheck: _publicKeyCheck,
-            txType: _txType,
-            txStatus: false,
-            value: _value
-        });
-        txList.push(txData);
-        beforeYYQtransaction(_seller, _value, msg.sender);
-        emit addTx(_seller, _fileNumberList, _publicKeyCheck);
-        return txList.length;
-    }
 
-    //购买定制数据数据
-    function buyRealTimeData(string memory _publicKeyCheck, string memory _ipOrEigenvalues, uint256 _value, uint32 _accountsNumber) public checkBalanceOfBuyRealTimeData(msg.sender, _value, _accountsNumber) returns(uint256){
-        txSaveRealTime memory txRealTimeData = txSaveRealTime({
-            buyer: msg.sender,
-            publicKeyCheck: _publicKeyCheck,
-            ipOrEigenvalues: _ipOrEigenvalues,
-            txStatus: false,
-            value: _value,
-            accountsNumber: _accountsNumber
-        });
-        txRealTimeList.push(txRealTimeData);
-        beforeSSDZtransaction(_value, msg.sender);
-        emit addRealTimeTx(_publicKeyCheck, _ipOrEigenvalues);
-        return txRealTimeList.length;
-    }
-
-    //确认非定制数据交易
-    function makeSureTx(uint256 i) public onlyOwnerTx(i){
-        if(txList[i].txType == 1){
-            afterYYQtransaction(txList[i].buyer, txList[i].seller, txList[i].value);
-        }
-        if(txList[i].txType == 2){
-            afterSYQtransaction(txList[i].buyer, txList[i].seller, txList[i].value);
-        }
-        emit confirmTx(i);
-    }
-
-    //设置连接账户
-    function setAddress(address[] memory addressList) public {
-        collectionAddress[msg.sender] = addressList;
-    }
-
-    //定制数据数据交易确认
-    function makeSureRealTimeTx(uint256 i) public onlyOwnerRealTimeTx(i){
-        for(uint256 j = 0; j < collectionAddress[msg.sender].length; j++){
-            afterSSDZtransaction(collectionAddress[msg.sender][j], txRealTimeList[i].value);
-        }
-        delete collectionAddress[msg.sender];
-        emit confirmRealTimeTx(i);
-    }
-
+    function () payable external { }
     //检查非定制数据交易的发起人
     function checkDataTxOwner(address buyer, uint256 i) public view returns(bool){
         if(buyer == txList[i].buyer){
@@ -122,6 +79,51 @@ contract TxControl is transaction{
         }else{
             return false;
         }
+    }
+
+    //添加非定制数据交易
+    // function buyData(address _seller, uint256[] memory _fileNumberList, string memory _publicKeyCheck, uint8 _txType, uint256 _value, uint8 _buyerGrade) public checkBalanceOfBuyData(msg.sender, _value) returns(uint256){
+    //     txSave memory txData = txSave({
+    //         buyer: msg.sender,
+    //         seller: _seller,
+    //         fileNumberList: _fileNumberList,
+    //         publicKeyCheck: _publicKeyCheck,
+    //         txType: _txType,
+    //         txStatus: false,
+    //         value: _value,
+    //         buyerGrade: _buyerGrade
+    //     });
+    //     txList.push(txData);
+    //     beforeDataTransaction(msg.sender, _value, _seller);
+    //     emit addTx(_seller, _fileNumberList, _publicKeyCheck);
+    //     return txList.length;
+    // }
+
+    function buyData(address _seller, uint8 _txType, uint256 _value, uint8 _buyerGrade) public checkBalanceOfBuyData(msg.sender, _value) returns(uint256){
+        txSave memory txData = txSave({
+            buyer: msg.sender,
+            seller: _seller,
+            txType: _txType,
+            txStatus: false,
+            value: _value,
+            buyerGrade: _buyerGrade
+        });
+        txList.push(txData);
+        beforeDataTransaction(msg.sender, _value, _seller);
+        // emit addTx(_seller, _fileNumberList, _publicKeyCheck);
+        return txList.length - 1;
+    }
+
+    //非定制数据数据交易退款
+    function refundData(uint256 i) public onlyOwnerTx(i) {
+        dataTransactionRefund(txList[i].buyer,txList[i].seller, txList[i].value);
+    }
+
+
+    //确认非定制数据交易
+    function makeSureTx(uint256 i) public onlyOwnerTx(i){
+        afterDataTransaction(txList[i].buyer,txList[i].seller, txList[i].value, txList[i].buyerGrade);
+        emit confirmTx(i);
     }
 
     //检查定制数据交易的发起人
@@ -133,20 +135,42 @@ contract TxControl is transaction{
         }
     }
 
-    //非定制数据数据交易退款
-    function refundData(uint256 i) public onlyOwnerTx(i) {
-        if(txList[i].txType == 1){
-            afterYYQtransaction(txList[i].buyer,txList[i].seller, txList[i].value);
-        }
-        if(txList[i].txType == 2){
-            afterSYQtransaction(txList[i].buyer, txList[i].value);
-        }
+    //购买定制数据数据
+    function buyRealTimeData(uint256 nonce, string memory _publicKeyCheck, string memory _ipOrEigenvalues, uint256 _value, uint32 _accountsNumber, uint8 _buyerGrade, uint64 _duration, uint256 _buyerId) public checkBalanceOfBuyRealTimeData(msg.sender, _value, _accountsNumber) returns(uint256, uint256){
+        txSaveRealTime memory txRealTimeData = txSaveRealTime({
+            buyer: msg.sender,
+            publicKeyCheck: _publicKeyCheck,
+            ipOrEigenvalues: _ipOrEigenvalues,
+            txStatus: false,
+            value: _value,
+            accountsNumber: _accountsNumber,
+            buyerGrade: _buyerGrade,
+            duration: _duration,
+            buyerId: _buyerId
+        });
+        txRealTimeList.push(txRealTimeData);
+        beforeRealTimeTransaction(_accountsNumber,_value, msg.sender );
+        emit addRealTimeTx(_buyerId, _publicKeyCheck, _ipOrEigenvalues, _duration);
+        return (txRealTimeList.length - 1, nonce);
     }
 
+    
+    //设置连接账户
+    function setAddress(address[] memory addressList) public {
+        collectionAddress[msg.sender] = addressList;
+    }
+
+    //定制数据数据交易确认
+    function makeSureRealTimeTx(uint256 i) public onlyOwnerRealTimeTx(i){
+        afterRealTimeTransaction(msg.sender, collectionAddress[msg.sender], txRealTimeList[i].value, txRealTimeList[i].buyerGrade);
+        delete collectionAddress[msg.sender];
+        emit confirmRealTimeTx(i);
+    }
+
+    
     // 定制数据数据交易退款
     function refundRealTime(uint256 i) public onlyOwnerRealTimeTx(i) {
-        uint256 number = collectionAddress[txRealTimeList[i].buyer].length;
-        afterSSDZtransaction(txRealTimeList[i].buyer, txRealTimeList[i].value * number);
+        realTimeTransactionRefund(txRealTimeList[i].buyer, collectionAddress[msg.sender], txRealTimeList[i].value);
     }
 
 }
--- contracts/transaction.sol
+++ contracts/transaction.sol
@@ -88,7 +88,7 @@ contract transaction is MyAdvancedToken {
     /**
      * buyer:买家地址,now:当前时间
      */
-    function _UpdateLastbuytime(address buyer, uint256 now)  internal whenNotPaused{
+    function _UpdateLastbuytime(address buyer)  internal whenNotPaused{
         _lastbuytime[buyer] = now;  //将账户设置为冻结状态
     }
 
@@ -96,59 +96,111 @@ contract transaction is MyAdvancedToken {
     /**
      * _to:卖家账户地址,totalprice:情报总价,_from:买家地址
      */ 
-    function beforeYYQtransaction(address _to, uint256 totalprice, address _from) public whenNotPaused{
-        require(!frozenAccount[_from]);             // 检查发送者账户是否冻结
-        require(!frozenAccount[_to]);               // 检查接收者账户是否冻结
-        buyerpayment = _Buyerpayment(totalprice);  // 计算买家付款
-        _transfer(_from, address(this), buyerpayment); //将买家付款转移到合约地址中
-        emit SuccessFunds(_from, true);
-  }
+    // function beforeYYQtransaction(address _to, uint256 totalprice, address _from) public whenNotPaused{
+    //     require(!frozenAccount[_from]);             // 检查发送者账户是否冻结
+    //     require(!frozenAccount[_to]);               // 检查接收者账户是否冻结
+    //     buyerpayment = _Buyerpayment(totalprice);  // 计算买家付款
+    //     _transfer(_from, address(this), buyerpayment); //将买家付款转移到合约地址中
+    //     emit SuccessFunds(_from, true);
+    // }
   
 //拥有权交易的实现（交易金额转给卖家）
     /**
      * state:交易状态，交易是否成功/确认收货或退款（true/false）,_from:买家地址,_to:卖家账户地址,totalprice:情报总价
      */ 
-    function afterYYQtransaction(uint8 grade, address _from, address _to, uint256 totalprice) public whenNotPaused{
-        //如果确认收货
-        sellercollection = _Sellercollection(totalprice);   //计算卖家收款
-        _transfer(address(this), _to, sellercollection); //将卖家收款从合约地址转移到卖家地址
-        currTimeInSeconds = _currTimeInSeconds(); //获取当前时间
-        _UpdateLastbuytime(_from, currTimeInSeconds); //更新买家最近一次购买情报的时间
-    }
+    // function afterYYQtransaction(uint8 grade, address _from, address _to, uint256 totalprice) public whenNotPaused{
+    //     //如果确认收货
+    //     sellercollection = _Sellercollection(totalprice);   //计算卖家收款
+    //     _transfer(address(this), _to, sellercollection); //将卖家收款从合约地址转移到卖家地址
+    //     currTimeInSeconds = _currTimeInSeconds(); //获取当前时间
+    //     _UpdateLastbuytime(_from, currTimeInSeconds); //更新买家最近一次购买情报的时间
+    // }
     
     
 //使用权交易的实现（交易金额的第三方托管）
-    function beforeSYQtransaction(address _to, uint256 totalprice, address _from) public whenNotPaused{
-        require(!frozenAccount[_from]);             // 检查发送者账户是否冻结
-        require(!frozenAccount[_to]);               // 检查接收者账户是否冻结
-        buyerpayment = _Buyerpayment(totalprice);  // 计算买家付款
-        _transfer(_from, address(this), buyerpayment); //将买家付款转移到合约地址中
-        emit SuccessFunds(_from, true);
-  }
+    // function beforeSYQtransaction(address _to, uint256 totalprice, address _from) public whenNotPaused{
+    //     require(!frozenAccount[_from]);             // 检查发送者账户是否冻结
+    //     require(!frozenAccount[_to]);               // 检查接收者账户是否冻结
+    //     buyerpayment = _Buyerpayment(totalprice);  // 计算买家付款
+    //     _transfer(_from, address(this), buyerpayment); //将买家付款转移到合约地址中
+    //     emit SuccessFunds(_from, true);
+    // }
   
 //使用权交易的实现（交易金额转给卖家）
-    function afterSYQtransaction(uint8 grade, address _from, address _to, uint256 totalprice) public whenNotPaused{
-        sellercollection = _Sellercollection(totalprice);   //计算卖家收款
-        _transfer(address(this), _to, sellercollection); //将卖家收款从合约地址转移到卖家地址
-        currTimeInSeconds = _currTimeInSeconds(); //获取当前时间
-        _UpdateLastbuytime(_from, currTimeInSeconds); //更新买家最近一次购买情报的时间
-    }
+    // function afterSYQtransaction(uint8 grade, address _from, address _to, uint256 totalprice) public whenNotPaused{
+    //     sellercollection = _Sellercollection(totalprice);   //计算卖家收款
+    //     _transfer(address(this), _to, sellercollection); //将卖家收款从合约地址转移到卖家地址
+    //     currTimeInSeconds = _currTimeInSeconds(); //获取当前时间
+    //     _UpdateLastbuytime(_from, currTimeInSeconds); //更新买家最近一次购买情报的时间
+    // }
     
 
 //实时定制交易的实现
-     function beforeSSDZtransaction(uint256 totalprice, address _from) public whenNotPaused{
-        require(!frozenAccount[_from]);             // 检查发送者账户是否冻结
-        buyerpayment = _Buyerpayment(totalprice);  // 计算买家付款
-        _transfer(_from, address(this), buyerpayment); //将买家付款转移到合约地址中
-        emit SuccessFunds(_from, true);
-  }
+    //  function beforeSSDZtransaction(uint256 totalprice, address _from) public whenNotPaused{
+    //     require(!frozenAccount[_from]);             // 检查发送者账户是否冻结
+    //     buyerpayment = _Buyerpayment(totalprice);  // 计算买家付款
+    //     _transfer(_from, address(this), buyerpayment); //将买家付款转移到合约地址中
+    //     emit SuccessFunds(_from, true);
+    // }
   
 //实时定制交易的实现（交易金额转给卖家）
-    function afterSSDZtransaction(uint8 grade, address _from, address _to, uint256 totalprice) public whenNotPaused{
-        sellercollection = _Sellercollection(totalprice);   //计算卖家收款
-        _transfer(address(this), _to, sellercollection); //将卖家收款从合约地址转移到卖家地址
-        currTimeInSeconds = _currTimeInSeconds(); //获取当前时间
-        _UpdateLastbuytime(_from, currTimeInSeconds); //更新买家最近一次购买情报的时间
+    // function afterSSDZtransaction(uint8 grade, address _from, address _to, uint256 totalprice) public whenNotPaused{
+    //     sellercollection = _Sellercollection(totalprice);   //计算卖家收款
+    //     _transfer(address(this), _to, sellercollection); //将卖家收款从合约地址转移到卖家地址
+    //     currTimeInSeconds = _currTimeInSeconds(); //获取当前时间
+    //     _UpdateLastbuytime(_from, currTimeInSeconds); //更新买家最近一次购买情报的时间
         
+    // }
+
+    
+
+
+
+
+
+    //非定制数据交易的实现（交易金额的第三方托管）  (对应TxControl合约中的添加非定制数据交易函数）
+    /**
+     * _to:卖家账户地址,totalprice:情报总价,_from:买家账户地址
+     */ 
+    function beforeDataTransaction(address _to, uint256 totalprice, address _from) public whenNotPaused{
+
+    }
+  
+//非定制数据交易的实现（交易金额转给卖家）    (对应TxControl合约中的确认非定制数据交易函数)
+    /**
+     * _from:买家账户地址,_to:卖家账户地址,totalprice:情报总价,buyergrade:买家用户等级
+     */ 
+    function afterDataTransaction(address _from, address _to, uint256 totalprice, uint8 buyergrade) public whenNotPaused{
+
+    }
+//非定制数据交易退款（交易金额退给买家）    (对应TxControl合约中的非定制数据数据交易退款函数)
+    /**
+     * _from:买家账户地址,_to:卖家账户地址(用来修改卖家的信用值),totalprice:情报总价
+     */ 
+    function dataTransactionRefund(address _from, address _to, uint256 totalprice) public whenNotPaused{
+
+    }
+
+//实时定制交易的实现（交易金额的第三方托管） (对应TxControl合约中的购买定制数据数据交易函数）
+    /**
+     * _to:卖家账户地址列表,value:每个卖家的情报价格,_from:买家账户地址
+     */ 
+    function beforeRealTimeTransaction(uint256 addressNumber, uint256 value, address _from) public whenNotPaused{
+
+    }
+  
+//实时定制交易的实现（交易金额转给卖家）  (对应TxControl合约中的定制数据数据交易确认函数）
+    /**
+     * _from:买家账户地址,_to:卖家账户地址列表,value:每个卖家的情报价格,buyergrade:买家用户等级
+     */ 
+    function afterRealTimeTransaction(address _from, address[] memory _to, uint256 value, uint8 buyergrade) public whenNotPaused{
+   
+    }
+//实时定制交易退款（交易金额退给买家）    (对应TxControl合约中的非定制数据数据交易退款函数)
+    /**
+     * _from:买家账户地址,_to:卖家账户地址列表(用来修改卖家的信用值),value:每个卖家的情报价格
+     */ 
+    function realTimeTransactionRefund(address _from, address[] memory _to, uint256 value) public whenNotPaused{
+
     }
 }
